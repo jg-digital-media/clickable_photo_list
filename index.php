0ca4103a93c1020340c94a53580a741cba6b64ae
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-11 08:21:34 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clickable Photo List</title>
+</head>
+<body>
+
+    <h1>Clickable Photo List</h1>
+    
+    <p>Click on the photos to see the details.</p>
+    
+</body>
+</html>
